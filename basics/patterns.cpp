8f71dd06cad661ba94041a008cf7e880631dae7e
Muhamed Marshad <muhamedmarshad18@gmail.com>
2024-06-24 12:14:33 +0530

    modified

---
+++ basics/patterns.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 4; i >= 0; i--)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            cout << n - j + 1 << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
