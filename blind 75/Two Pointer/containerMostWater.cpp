3f0e4d877212843d7d7fe8fb31cc884cf9b9c129
Muhamed Marshad <muhamedmarshad18@gmail.com>
2024-07-04 10:46:45 +0530

    Add file

---
+++ blind 75/Two Pointer/containerMostWater.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int maxArea(vector<int> &height)
+{
+    int left = 0;
+    int right = height.size() - 1;
+    int max_area = 0;
+
+    while (left < right)
+    {
+        int width = right - left;
+        int current_height = min(height[left], height[right]);
+        int current_area = width * current_height;
+        max_area = max(max_area, current_area);
+
+        // Move the pointer pointing to the shorter line
+        if (height[left] < height[right])
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+
+    return max_area;
+}
+
+int main()
+{
+    vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    cout << "Maximum amount of water the container can store is: " << maxArea(height) << endl;
+    return 0;
+}
