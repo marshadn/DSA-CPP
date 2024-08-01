d554d40fd54d0f4c930292376cb9d004d54d267b
marshadn <muhamedmarshad18@gmail.com>
2024-08-01 21:41:07 +0530

    Add file

---
+++ Two Pointer/lonsSubstr.cpp
@@ -0,0 +1,34 @@
+// length of longest susstrting without repeating character
+
+#include <bits/stdc++.h>
+using namespace std;
+int longestUniqueSubsttr(string s)
+{
+    vector<int> mpp(256, -1);
+
+    int left = 0, right = 0;
+    int n = s.size();
+    int len = 0;
+    while (right < n)
+    {
+        if (mpp[s[right]] != -1)
+            left = max(mpp[s[right]] + 1, left);
+
+        mpp[s[right]] = right;
+
+        len = max(len, right - left + 1);
+        right++;
+    }
+    return len;
+}
+
+int main()
+{
+    string str = "geeksforgeeks";
+    cout << "The input string is " << str << endl;
+    int len = longestUniqueSubsttr(str);
+    cout << "The length of the longest non-repeating "
+            "character substring is "
+         << len;
+    return 0;
+}
