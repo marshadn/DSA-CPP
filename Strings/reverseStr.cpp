fe93a6845dd22f8f7dd32bf0c53a73b221fd35ca
marshadn <muhamedmarshad18@gmail.com>
2024-09-02 21:52:48 +0530

    Add more string problems

---
+++ Strings/reverseStr.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    string str = "HELLO";
+
+    reverse(str.begin(), str.end());
+
+    cout << "The reversed string is- " << str;
+}
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void swap(string & a, int i, int j) {
+  char temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
+}
+
+void reverseStr(string & a) {
+  int i = 0;
+  int j = a.length() - 1;
+
+  while (i < j) {
+    swap(a, i, j);
+    i++;
+    j--;
+  }
+}
+
+int main() {
+
+  string str = "HELLO";
+  reverseStr(str);
+  cout << "The reversed string is- " << str;
+
+  return 0;
+}
+*/
