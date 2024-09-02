8ed0f09371200509119132213d1a77697cfe676e
marshadn <muhamedmarshad18@gmail.com>
2024-09-02 21:47:28 +0530

    Add more string problems

---
+++ Strings/removeSpaces.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+string removeSpaces(char str[])
+{
+    int count = 0; // spaces seen so far
+
+    for (int i = 0; str[i]; i++)
+        if (str[i] != ' ')
+        {                        // if whitespace is present
+            str[count] = str[i]; // remove whitespace
+            count++;             // increment the count
+        }
+
+    str[count] = '\0';
+
+    return str;
+}
+
+int main()
+{
+    char str[] = "looks into forward";
+    cout << removeSpaces(str);
+
+    return 0;
+}
