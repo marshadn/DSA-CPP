2fcd41f6668181d75a0a8fb321815f63b5d74468
marshadn <muhamedmarshad18@gmail.com>
2024-09-03 06:45:31 +0530

    Add capitalize 1st last str

---
+++ Strings/capitalize.cpp
@@ -0,0 +1,34 @@
+// Capitalize first and last character of each word of a string
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Capitalize(string str, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (i == 0 || i == (size - 1)) // Converting first and last index character to uppercase
+            {
+                str[i] = towupper(str[i]);
+            }
+        else if (str[i] == ' ') // Converting characters present before and after space to uppercase
+            {
+                str[i - 1] = towupper(str[i - 1]);
+                str[i + 1] = towupper(str[i + 1]);
+            }
+    }
+
+    cout << "String after capitalizing the first and last letter of each word of the string : " << "\n ";
+    cout << str << "\n";
+}
+int main()
+{
+
+    // Input string
+    string str = "cet is good";
+    int size = str.length();
+
+    Capitalize(str, size);
+    return 0;
+}
