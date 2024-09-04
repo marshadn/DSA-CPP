a6ca200645a11c42e480d4494d8c80d82f42a589
marshadn <muhamedmarshad18@gmail.com>
2024-09-04 06:00:15 +0530

    Modify code using left shift

--- Bit-Manipulation/setBit.cpp
+++ Bit-Manipulation/setBit.cpp
@@ -1,22 +1,42 @@
-// check if ith bit is set or not
+// // check if ith bit is set or not
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-
-    int n;
-    cout << "Enter the number:";
-    cin >> n;
+    int n = 13;
     int i;
-    cout << "Enter the index:";
+    cout << "Enter the index : ";
     cin >> i;
-    if ((n >> i) & 1)
+    if ((1 << i) & n)
     {
-        cout << n << " is set";
+        cout << i << " th bit is set";
     }
     else
     {
-        cout << n << " is unset";
+        cout << i << " th bit NOT set";
     }
     return 0;
 }
+// OR
+//  #include <bits/stdc++.h>
+//  using namespace std;
+//  int main()
+//  {
+
+//     int n;
+//     cout << "Enter the number:";
+//     cin >> n;
+//     int i;
+//     cout << "Enter the index:";
+//     cin >> i;
+//     if ((n >> i) & 1)
+//     {
+//         cout << n << " is set";
+//     }
+//     else
+//     {
+//         cout << n << " is unset";
+//     }
+//     return 0;
+// }
