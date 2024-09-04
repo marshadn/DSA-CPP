02f0e8de174c3b236808e593bd4c2e6bf07b1869
marshadn <muhamedmarshad18@gmail.com>
2024-09-04 06:54:51 +0530

    Modify code

--- Bit-Manipulation/count1Bit.cpp
+++ Bit-Manipulation/count1Bit.cpp
@@ -1,26 +1,48 @@
-// count the set bits(count coccurence of 1)
-//  clear the ith bit(i.e, ith position bit turn into 0)
+// // count the set bits(count coccurence of 1)
+// //  clear the ith bit(i.e, ith position bit turn into 0)
+
 #include <bits/stdc++.h>
 using namespace std;
-int countSetBits(int n)
-{
-    int count = 0;
-    while (n != 0)
-    {
-        if (n % 2 == 0)
-        {
-            n = n & (n - 1);
-            count++;
-        }
-    }
-    return count;
-}
 int main()
 {
     int n;
-    cout << "Enter the number:";
     cin >> n;
-    cout << "The no.of count bits:" << countSetBits(n);
-
+    int cnt = 0;
+    while (n > 1)
+    {
+        if (n % 2 == 1)
+            cnt++;
+        n /= 2;
+        if (n == 1)
+            cnt++;
+    }
+    cout << cnt;
     return 0;
 }
+
+
+                //OR
+// #include <bits/stdc++.h>
+// using namespace std;
+// int countSetBits(int n)
+// {
+//     int count = 0;
+//     while (n != 0)
+//     {
+//         if (n % 2 == 0)
+//         {
+//             n = n & (n - 1);
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// int main()
+// {
+//     int n;
+//     cout << "Enter the number:";
+//     cin >> n;
+//     cout << "The no.of count bits:" << countSetBits(n);
+
+//     return 0;
+// }
