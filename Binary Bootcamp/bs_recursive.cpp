47c943d74dd78dbe126f961661d3b8699190ecd5
marshadn <muhamedmarshad18@gmail.com>
2024-09-08 07:44:26 +0530

    Binary Search using recursion

---
+++ Binary Bootcamp/bs_recursive.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int bs(vector<int> &arr, int low, int high, int target)
+{
+    int mid = (low + high) / 2;
+    if (low > high)
+        return -1;
+    else if (arr[mid] == target)
+        return mid;
+    else if (arr[mid < target])
+        return bs(arr, low + 1, high, target);
+    else
+        return bs(arr, low, high - 1, target);
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 4, 5, 8, 29};
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int target;
+    cout << "Enter the target  value:";
+    cin >> target;
+    int res = bs(arr, 0, n - 1, target);
+    if (res == -1)
+        cout << target << " NOT found!!" << endl;
+    else
+        cout << target << " is found at index " << res;
+    return 0;
+}
