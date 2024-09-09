f23cb3caa0387e8f3200761f90ac4e3d0c36791d
marshadn <muhamedmarshad18@gmail.com>
2024-09-09 11:55:22 +0530

    Add file

---
+++ Z-basics/str.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // cout<<"\n";
+	// your code goes here
+// 	string str="Hello world ";
+// 	cout<<"inedx : "<<str.at(8)<<endl;
+// 	for(char &c:str){
+// 	 c=toupper(c);
+// 	cout<<c;
+// 	}
+// 	cout<<"\n";
+// 	for(char &a:str){
+// 	a=tolower(a);
+// 	cout<<a;
+// 	}
+// 	cout<<"\n";
+// 	cout<<str.substr(0,str.length()-1)<<endl;;
+// 	cout<<str.find('o')<<endl;
+// 	cout<<str.replace(0,5,"Malare")<<endl;
+//	string str2="My life";
+// 	cout<<str.append(str2)<<endl;
+// 	if(str.compare(str2)<0){
+// 	    cout<<"Worked";
+// 	}
+// 	else{
+// 	    cout<<"Not worked";
+// 	}
+// cout<<endl;
+// char d='a';
+// cout<<(int)d<<endl;
+// reverse(str.begin(),str.end());
+// cout<<str;
+//swap(str,str2);
+// string str3="Marshad";
+// cout<<str3.substr(1,str3.size()-2)<<endl;
+// cout<<"Before: "<<str<<" "<<str2;
+// str.swap(str2);
+// cout<<endl;
+// cout<<"After: "<<str<<" "<<str2 ;
+
+    // vector<string> x={"manga","Thenga","chakkas","chakka","chakkamasdt"};
+    // sort(x.begin(),x.end());
+    // reverse(x.begin(),x.end());
+    // for(int i=0;i<x.size();i++){
+    //     cout<<x[i]<<" ";
+    // }
+    
+}
